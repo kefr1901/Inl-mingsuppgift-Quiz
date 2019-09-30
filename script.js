c361b8136c7fee169784e6cadbe7583abb6063e5
kefri1901 <kevin.fridman@yh.nackademin.se>
2019-09-30 12:23:46 +0200

    added changes so it can read JSON files

--- script.js
+++ script.js
@@ -13,15 +13,19 @@ class Quiz {
 
         // kolla vilken checkbox som är ikryssad och om det är rätt svar, fortsätter till nästa fråga
         //med hjälp av att this.currentQuestion byter value från 0-1 vid första frågan. 
-
+        
         if (this.currentQuestion + 1 >= this.questions.length) {
             console.log("DU KLARADE DET");
         } else {
-            if (document.getElementById("checkbox" + (this.questions[this.currentQuestion].correct + 1)).checked){
+            
+
+            if (document.getElementById("checkbox" + (this.questions[this.currentQuestion].correct[0])).checked){
+                console.log("RÄTT SVAR");
                 this.currentQuestion++;
                 this.nextQuestion();
 
-                console.log("RÄTT SVAR");
+                
+                
             }
         }
 
@@ -34,6 +38,7 @@ class Quiz {
         
 
         document.getElementById("question").innerHTML = quiz.questions[quiz.currentQuestion].question;
+        document.getElementById("category").innerHTML =  "Din kategori är: " + quiz.questions[quiz.currentQuestion].category;
         document.getElementById("a1").innerHTML = quiz.questions[quiz.currentQuestion].answers[0];
         document.getElementById("a2").innerHTML = quiz.questions[quiz.currentQuestion].answers[1];
         document.getElementById("a3").innerHTML = quiz.questions[quiz.currentQuestion].answers[2];
@@ -54,13 +59,21 @@ class Quiz {
         }
     }
 }
+
+class Question{
+    constructor(category,question,answers,correct){
+
+        this.category = category;
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+
+    }
+}
+
+
+
  
-
-
-
-
-
-
 
 
 let name = prompt("skriv in ditt namn för att starta spelet!"); //frågar om namnet och ger den en variabel
@@ -70,17 +83,27 @@ alert("hej" + quiz.name + "du har valt att spela med " + quiz.howManyQuestions_a
 
 
 
+let json = getJSON("http://www.mocky.io/v2/5d8dfd703100000e9c2b524b")
+
+for(let question of json ){
+    
+    quiz.questions.push(new Question(question.category, question.question, question.answers,question.correct));
+    
+}
+
+//console.log(question.category);
 
 
-quiz.questions.push(question1, question2, question3, question4); //pushar in frågorna i classen Quiz från objektet 
+
+
+
+//quiz.questions.push(question1, question2, question3, question4); //pushar in frågorna i classen Quiz från objektet 
 //med frågor fråm klassen questions.
 
 // kör spelet, för att få igång första frågan
 
 quiz.howManyQuestions();
 quiz.nextQuestion();
-
-
 
 
 
